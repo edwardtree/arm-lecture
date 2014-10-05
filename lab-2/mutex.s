de3b4ff7ffeab190ac2c7ee24ef62343089797da
edwardtree <edwardsmalltree@yahoo.com.tw>
2014-10-05 14:13:59 +0800

    add loop .detectlock in mutex.s and finish, fix loop 00FFFFFF in atomic.c

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -10,15 +10,16 @@
 lock_mutex:
         ldr r1, =locked @ load locked value into r1 the temp register 
 	@ INSERT CODE BELOW
-	
-	ldrex r2,[r0] 
+		
+.detectlock:
+	ldrex r2,[r0]	
 	cmp r2, r1
-	beq lock_mutex @if lock , loop again
-
-	strexeq r2,r1,[r0] @ store r1 into r0, if successful r2 equal zero 
-	cmp r2, #0
-	bne lock_mutex @if lock fail , lock again
-
+	beq .detectlock @if lock , loop again
+	
+	strex r2,r1,[r0] @ store r1 into r0, if successful r2 equal zero 
+	cmp r2, #1 
+	beq .detectlock @if lock fail , loop again
+	DMB
         @ END CODE INSERT
 	bx lr
 
@@ -29,8 +30,8 @@ lock_mutex:
 unlock_mutex:
 	@ INSERT CODE BELOW
         ldr r1, =unlocked
+	DMB
 	str r1,[r0] @ the thread finish work and unlock key
-	bx lr
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
