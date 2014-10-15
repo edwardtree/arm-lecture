e6c89af1f37f8e71f0447704746ec1c4f7d482c2
edwardtree <edwardsmalltree@yahoo.com.tw>
2014-10-15 11:59:45 +0800

    fix non-recursive fib.s, but still not work

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -4,53 +4,32 @@
 	.align 2
 	.thumb
 	.thumb_func
-
 	.global fibonacci
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	subs r4, r0, #0
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-	ble .L3
+	push {r4, r5, r6, r7, lr}
 
+	add r4, r4, #-1
+	add r5, r5, #1
+	add r7, r7, #0
+	add r6, r6, #0
 
-	@ Compare R4 wtih 1
-	cmp r4, #1
-	@ If R4 == 1 goto .L4 (which returns 1)
-	beq .L4
+L1:
+	
+	add r6, r5, r4
+	mov r4, r5
+	mov r5, r6
+	
+	add r7, r7, #1
+	cmp r7, r0
+	ble L1
 
-
-	@ R0 = R4 - 1
-	subs r0, r4, #1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-	bl fibonacci
-
-
-	@ R5 = R0
-	mov r5, r0 
-	@ R0 = R4 - 2
-	subs r0, r4, #2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-	bl fibonacci
-
-
-	@ R0 = R5 + R0 (update flags)
-	add r0, r5, r0
-	pop {r3, r4, r5, pc}		@EPILOG
+	mov r0, r5 
+	pop {r4, r5, r6, r7,pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
